283397d5ca4bba5719ef176f08376ca58f0c10d0
David Serrano <davidserrano@cis.vvc.edu>
2018-10-18 11:33:00 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,14 +1,41 @@
 /**
-Description:
-Filename:
+Description: 
+Filename: main.cpp
 Author:
-Date:
+Date: 
 */
+
+#include <iostream>
+#include <string>
 
 using namespace std;
 
+int char_count(string p1, char c)
+{
+    int count = 0;
+    for(int i = 0; i < p1.size(); i++)
+    {
+    if(p1.at(i) == c){ count++;}
+    }
+    return count;
+}
+
+
+string get_string(string prompt)
+{
+    string input;
+    cout << prompt;
+    cin >> input;
+    return input;
+}
 
 int main()
-{
-   return 0;
+{ 
+    string input = get_string("Please enter a string: ");
+    char char_to_check;
+    cout << "Now please enter a character to count: ";
+    cin >> char_to_check;
+
+    cout<< char_count(input, char_to_check);
+    return 0;
 }
