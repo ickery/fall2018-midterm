64dd55cce473f666a9fcb844bc0ae8313f9bc9da
David Serrano <davidserrano@cis.vvc.edu>
2018-10-18 12:07:31 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 /**
-Description: 
+Description: Program that asks the user for a string and character to preform a count on the amoung of characters in the string. D
 Filename: main.cpp
-Author:
-Date: 
+Author: David Serrano
+Date: 10/18/22
 */
 
 #include <iostream>
@@ -11,12 +11,18 @@ Date:
 
 using namespace std;
 
+// Checks whether two given chars are equivalent
+// @Param1 char to be checked
+// @Param2 char to be checked
 bool is_char(char c, char is_char)
 {
     if(tolower(c) == tolower(is_char)){ return true; }
     return false;
 }
 
+// Counts the amount of a character in a given string
+// @Param1 string to be checked
+// @Param2 character to search for
 int char_count(string p1, char c)
 {
     int count = 0;
@@ -28,6 +34,8 @@ int char_count(string p1, char c)
 }
 
 
+// Display a prompt followed by a request to input a string which is returned
+// @Param prompt displayed before asking user for string
 string get_string(string prompt)
 {
     string input;
@@ -44,6 +52,6 @@ int main()
     cout << "Now please enter a character to count: ";
     cin >> char_to_check;
 
-    cout<< char_count(input, char_to_check);
+    cout << "There are " <<  char_count(input, char_to_check) << " " << char_to_check << "'s in the string." <<  endl;
     return 0;
 }
